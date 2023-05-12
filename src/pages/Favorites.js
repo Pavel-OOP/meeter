e2791f0f13339ec0a22c44d739bec51dd1b43172
Pavel-OOP <pavel.i.petrov.7@gmail.com>
2023-05-12 15:47:42 +0300

    sc

---
+++ src/pages/Favorites.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Component({ value, onChange }) {
+    const [state, setState] = useState('')
+  
+    return <div>...</div>
+  }
